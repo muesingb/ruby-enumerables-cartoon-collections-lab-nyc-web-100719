7da49de66881ee436c480f209a7eb2a5f441f96a
Barbara Muesing <muesingb@gmail.com>
2019-09-30 16:22:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,5 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.detect do |item| cheese_types.include?(item)
-  end
-  #{|i| i.include?(cheese_types)}
+  food.detect {|item| cheese_types.include?(item)}
 end
